cbb7d99aab9afd5ecdfd1221e45e8eaf463fb7c1
baldy <b.a.roper@soton.ac.uk>
2017-08-25 16:57:51 +0100

    added enhanced output using various parameters

--- dTree.py
+++ dTree.py
@@ -1,20 +1,46 @@
 
-import sys
-from time import time
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 import data
 
 
-trainingTargets, trainingFeatures=data.processData('output_iow.csv')
-testTargets, testFeatures = data.processData('results.csv')
+v=19
 
 
-dt = tree.DecisionTreeClassifier( )
-dt.fit(trainingFeatures,trainingTargets)
+print "\n"
+def runMLFullMFD(v):
+    trainingTargets, trainingFeatures = data.processData('winSet.csv')
+    testTargets, testFeatures = data.processData('iowAset.csv')
+    dt = tree.DecisionTreeClassifier()
+    dt.fit(trainingFeatures, trainingTargets)
+    dtPred = dt.predict(testFeatures)
+    acc = accuracy_score(dtPred, testTargets)
+
+    print "*****************************************************"
+    print "Accuracy using MFD, #vertices, #edges and diameter"
+    print " params: max_depth=" + str(v)
+    print acc
+    print "*****************************************************"
+
+def runNumVE_D(v):
+    trainingTargets2, trainingFeatures2 = data.processDataNoMFD('winSet.csv')
+    testTargets2, testFeatures2 = data.processDataNoMFD('iowAset.csv')
+    dt2 = tree.DecisionTreeClassifier(max_leaf_nodes=2)
+    dt2.fit(trainingFeatures2, trainingTargets2)
+    dtPred2 = dt2.predict(testFeatures2)
+    acc2 = accuracy_score(dtPred2, testTargets2)
+
+    print "*****************************************************"
+    print "Accuracy using just #vertices, #edges and diameter"
+    print " params: max_depth=" + str(v)
+    print acc2
+    print "*****************************************************"
+
+for v in range(0,25,1):
+    runMLFullMFD(v)
 
 
-dtPred = dt.predict(testFeatures)
-acc = accuracy_score(dtPred, testTargets)
-print acc
 
+
+#tree.export_graphviz(dt,out_file=data.datapath+"treeMFD.dot")
+#tree.export_graphviz(dt,out_file=data.datapath+"treeNoMFD.dot")
--- data.py
+++ data.py
@@ -12,3 +12,14 @@ def processData(filename):
    # print tFeatures[0]
     #print tFeatures
     return tTargets,tFeatures
+
+
+def processDataNoMFD(filename):
+    osmData = loadtxt(datapath + filename, delimiter=',')
+    tTargets = osmData[:, 0]
+    tFeatures = osmData[:, 17:]
+    #data={tTargets,tFeatures}
+    ##print osmData.shape
+   # print tFeatures[0]
+    #print tFeatures
+    return tTargets,tFeatures
